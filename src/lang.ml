331058bf9c0da8b7a59dd96017af5ddf4bf04ac8
Samuel Mimram <smimram@gmail.com>
2006-12-06 12:53:05 +0000

    More natural orientation for braids.

--- src/lang.ml
+++ src/lang.ml
@@ -148,11 +148,11 @@ object (self)
             let q = Wire.new_polyline (c.(1)::pos::c.(2)::[]) in
               p::q::[]
         | "braid" ->
-            let p1 = Wire.new_polyline (c.(0)::(circle_position pos c.(0))::[]) in
-            let l = new Wire.line (circle_position pos c.(0)) (circle_position pos c.(3)) in
-            let p2 = Wire.new_polyline ((circle_position pos c.(3))::c.(3)::[]) in
-            (* let q = Wire.new_polyline (c.(1)::pos::c.(2)::[]) in *)
-            let q = Wire.new_polyline (c.(1)::(circle_position pos c.(1))::(circle_position pos c.(2))::c.(2)::[]) in
+            let p1 = Wire.new_polyline (c.(1)::(circle_position pos c.(1))::[]) in
+            let l = new Wire.line (circle_position pos c.(1)) (circle_position pos c.(2)) in
+            let p2 = Wire.new_polyline ((circle_position pos c.(2))::c.(2)::[]) in
+            (* let q = Wire.new_polyline (c.(0)::pos::c.(3)::[]) in *)
+            let q = Wire.new_polyline (c.(0)::(circle_position pos c.(0))::(circle_position pos c.(3))::c.(3)::[]) in
               l#add_attr "opacity" "0";
               p1#append_line l;
               p1#append p2;
