7395877870033fb3034d1d603594ad59bbd45c1e
Samuel Mimram <smimram@gmail.com>
2007-08-19 16:27:13 +0000

    Document a bit.

--- src/lang.mli
+++ src/lang.mli
@@ -1,21 +1,48 @@
+(** An environment. *)
 type environment = (string * string) list
 
+(** An option. *)
 type opt = string * (string * string) list
 
+(** Convert a string representing a relative direction into a relative
+  * direction. *)
 val reldir_of_string : string -> float * float
 
+(** [box name dirs opts] creates a new object representing an operation of kind
+  * [name] with [dirs] as relative positions for inputs and outputs and [opt] as
+  * options. *)
 class box : string -> Wire.reldir list -> opt list ->
 object
+  (** Set the environment of the box. *)
   method set_env : environment -> unit
+
+  (** Get the decorations for the labels, given the position of the box. *)
   method get_label_decorations : float * float -> Wire.rectangle list
+
+  (** Get the polylines for the strings of the box. *)
   method get_plines : Wire.dir -> Wire.polyline list
+
+  (** Get the texts of the labels. *)
   method get_texts : float * float -> Wire.text list
+
+  (** Get the kind of the box. *)
   method kind : string
 end
 
-type line = box option list
-type ir_matrix = line list
-type matrix = box option array array
+(** A relative direction. *)
 type dir = Left | Right | Up | Down
-val matrix_of_ir : environment -> box option list list -> box option array array
-val process_matrix : Wire.output_kind -> environment -> box option array array -> string
+
+(** A line of the matrix. *)
+type line = box option list
+
+(** Intermediate representation of the matrix. *)
+type ir_matrix = line list
+
+(** Abstract representation of a matrix. *)
+type matrix = box option array array
+
+(** Get a matrix from the intermediate representation. *)
+val matrix_of_ir : environment -> ir_matrix -> matrix
+
+(** Generate the image from a matrix. *)
+val process_matrix : Wire.output_kind -> environment -> matrix -> string
